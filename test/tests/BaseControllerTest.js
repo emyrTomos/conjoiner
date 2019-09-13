50c1b7ba46d72b6cc01b94d7ee2f5542a1ac6a0e
Emyr Tomos <emyr.tomos@gmail.com>
2019-09-13 18:40:17 +0100

    Improved variable names and test descriptions ready to test function events

--- test/tests/BaseControllerTest.js
+++ test/tests/BaseControllerTest.js
@@ -7,54 +7,54 @@ describe("BaseController", function() {
 	model.getMemberByTitle = title => {return model.members.reduce(member => member.title === title)}
 	const el = document.createElement('span')
 	document.body.appendChild(el)
-	let receivedEvent
+	let propertyChangeEvent
 	el.addEventListener('propertyChange', function(ev){
-		receivedEvent = ev
-		console.log('received event ', receivedEvent)
+		propertyChangeEvent = ev
+		console.log('received event ', propertyChangeEvent)
 	})
 	const controller = new conjoiner.BaseController(model)
-	const binding = {target: el, event: 'propertyChange', property: 'title'}
+	const propertyBinding = {target: el, event: 'propertyChange', property: 'title'}
 	let lenBindings
 	beforeEach(function(){
 		lenBindings = controller.bindings.length
 	})
 	describe('#addBinding', function(){
 		it("should add a binding to the bindings array", function() {
-			controller.addBinding(binding)
+			controller.addBinding(propertyBinding)
 			controller.bindings.length.should.equal(lenBindings + 1)
 		});
 	})
 	describe('#hasBinding', function(){
 		it('is expected to return true for binding', function(){
-			expect(controller.hasBinding(binding)).to.be.true
+			expect(controller.hasBinding(propertyBinding)).to.be.true
 		})
 	})
 	describe('#removeBinding', function(){
 		it("should remove an element from the bindings", function() {
-			controller.removeBinding(binding)
+			controller.removeBinding(propertyBinding)
 			controller.bindings.length.should.equal(lenBindings - 1)
 		});
 	})
 	describe('#hasBinding', function(){
 		it('is expected to return false for binding', function(){
-			expect(controller.hasBinding(binding)).to.be.false
+			expect(controller.hasBinding(propertyBinding)).to.be.false
 		})
 	})
 	describe('#notify', function(){
-		it('element bound to propertyChange should receive an event when the field it is bound to is modified', function(){
-			controller.addBinding(binding)
-			should.not.exist(receivedEvent)
+		it('element bound to propertyChange should receive an event when the property it is bound to is modified', function(){
+			controller.addBinding(propertyBinding)
+			should.not.exist(propertyChangeEvent)
 			controller.model.title = "Modified String"
-			should.exist(receivedEvent)
+			should.exist(propertyChangeEvent)
 		})
-		it('event field should be propertyChange', function(){
-			receivedEvent.detail.event.should.equal('propertyChange')
+		it('event detail "event" property should be propertyChange', function(){
+			propertyChangeEvent.detail.event.should.equal('propertyChange')
 		})
-		it('property field should be property modified', function(){
-			receivedEvent.detail.property.should.equal('title')
+		it('event detail "property" property should be the property modified', function(){
+			propertyChangeEvent.detail.property.should.equal('title')
 		})
-		it('value field should be the value that was set on the property', function(){
-			receivedEvent.detail.value.should.equal("Modified String")
+		it('event detail "value" property should be the value that was set on the property', function(){
+			propertyChangeEvent.detail.value.should.equal("Modified String")
 		})
 	})
 
