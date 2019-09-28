5c47bbe79cbb22051166a6a7eff7315d14d19739
Emyr Tomos <emyr.tomos@gmail.com>
2019-09-28 14:23:34 +0100

    Added an index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+conjoiner = {}
+import BaseController from './BaseController'
+import Notifier from './Notifier'
+conjoiner.BaseController = BaseController
+conjoiner.Notifier = Notifier
+export default conjoiner
+/*const classes = {}
+classes.BaseController = require('./classes/BaseController')
+classes.Notifier = require('./classes/Notifier')
+module.exports = classes
+*/
