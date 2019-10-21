5fdbd507672ac9b7ba84f7bf35b9a67d60515e34
Emyr Tomos <emyr.tomos@gmail.com>
2019-10-21 04:17:54 +0100

    Conflict resolution

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const classes = {}
+const conjoiner = {}
+import BaseController from './classes/BaseController'
+import Notifier from './classes/Notifier'
+conjoiner.BaseController = BaseController
+conjoiner.Notifier = Notifier
+export conjoiner conjoiner
+export BaseController BaseController
+export Notifier Notifier
+/*const classes = {}
+>>>>>>> Stashed changes
 classes.BaseController = require('./classes/BaseController')
 classes.Notifier = require('./classes/Notifier')
 module.exports = classes
